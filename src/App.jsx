3fc12dc5eccab7c3d66432404aa005c13ec3e826
yujoesonic <yujoessj@gmail.com>
2023-11-27 09:16:11 +0800

    final

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
+import { useRef } from "react";
+
+function App() {
+  const WebgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    WebgiViewerRef.current.triggerPreview();
+  }
+
+  return (
+    <div className="App">
+      <div ref={contentRef} id="content">
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={WebgiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
